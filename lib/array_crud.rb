28ea60fa88ed4758561caed45507547580c904d4
Sara Nolen <nolensara1@gmail.com>
2020-11-29 17:35:27 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  array = ["wow", "I", "am", "really", "learning"]
+  #array = ["wow", "I", "am", "really", "learning"]
   array(element) << "arrays!"
 
 end
